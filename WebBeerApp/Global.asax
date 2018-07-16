bd12aa5d4cd9b285d59093c6aab1c3df91580b41
Amadeusz <amadeusz.przybyla@gmail.com>
2018-07-16 16:35:26 +0200

    open project

---
+++ WebBeerApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebBeerApp.MvcApplication" Language="C#" %>
---
+++ WebBeerApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebBeerApp.Startup))]
+namespace WebBeerApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
